daa130241ab6fb7c939477c2dc1fd3fbaa1c9273
Senri Yoshikawa <s13204@tokyo.kosen-ac.jp>
2017-07-28 15:41:30 +0900

    全ての点を１枚のグラフにプロットする

--- scatter/main.py
+++ scatter/main.py
@@ -14,52 +14,54 @@ for i in range(2, len(sys.argv)) :
     s_row = next(reader)
     w_row = next(weather)
 
-    s1 = s2 = w1 = w2 = []
-
-    #print(s_row[0] + " " + s_row[1] + " " + w_row[0] + " " + w_row[1])
-
+    weatherfile.seek(0)
     while s_row[0] != w_row[0]:
         w_row = next(weather)
 
-        #print(s_row[0] + " " + s_row[1] + " " + w_row[0] + " " + w_row[1])
+    s1 = []
+    s2 = []
+    w1 = []
+    w2 = []
 
     infile.seek(0)
     for row in reader:
         w_row[3] = w_row[3].replace(' )', '')
         w_row[3] = w_row[3].replace(' ]', '')
-        #print(w_row[0] + " " + w_row[1] + " " + w_row[3])
-        if len(row) > 2 and w_row[3] != "///":
-            if row[2] == '0' or row[2] == 'x' or row[2] == 'X':
-                continue
-            s1.append(0 if(row[2] == 'x' or row[2] == 'X') else int(row[2], 16))
+        if len(row) > 2 and w_row[3] != "///" and row[2] != 'x' and row[2] != 'X':
+            s1.append(0 if (row[2] == 'x' or row[2] == 'X') else row[2])
             w1.append(float(w_row[3]))
-            print(row[2] + " " + w_row[3])
-        if len(row) > 3 and w_row[3] != "///":
-            if row[3] == '0' or row[3] == 'x' or row[3] == 'X':
-                continue
-            s2.append(0 if(row[3] == 'x' or row[3] == 'X') else int(row[3], 16))
+        if len(row) > 3 and w_row[3] != "///" and row[3] != 'x' and row[3] != 'X':
+            s2.append(0 if (row[3] == 'x' or row[3] == 'X') else row[3])
             w2.append(float(w_row[3]))
+
         try:
             w_row = next(weather)
-        except:
+        except Exception as e:
+            print('file out range. ', e)
             break
+
     infile.close()
 
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
-    ax1.scatter(s1, w1)
-    ax1.set_title('first scatter plot')
+    ax1.scatter(s1, w1, alpha=0.2, s=0.5)
+    ax1.set_title('living sensor templature scatter')
     ax1.set_xlabel('sensor1')
-    ax1.set_ylabel('weather')
-    #fig1.show()
+    ax1.set_ylabel('templature[deg]')
+    ax1.set_xlim(0,)
+    ax1.set_ylim(-15, 40)
+    # fig1.show()
 
     ax2 = fig.add_subplot(1, 2, 2)
-    ax2.scatter(s2, w2)
-    ax2.set_title('first scatter plot')
+    ax2.scatter(s2, w2, alpha=0.2, s=0.5)
+    ax2.set_title('entrance sensor templature scatter')
     ax2.set_xlabel('sensor2')
-    ax2.set_ylabel('weather')
-    #fig2.show()
+    ax2.set_ylabel('templature[deg]')
+    ax2.set_xlim(0,)
+    ax2.set_ylim(-15, 40)
+    # fig2.show()
 
-    plt.savefig("test.png")
+    fig.tight_layout()
+    plt.savefig(sys.argv[i][0:-4] + ".png")
 
 weatherfile.close()
